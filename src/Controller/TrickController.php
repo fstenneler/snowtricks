82d138226eb91493e2acc43ab62f18d71755d9fe
fstenneler <fabien.stenneler@gmail.com>
2019-10-03 18:29:07 +0200

    fixed other Symfony Insights issues

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -4,14 +4,15 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class TrickController extends AbstractController
 {
+    /**
+     * @Route("/", name="home", methods={"GET","HEAD"})
+     */
     public function index()
     {
-        /**
-         * @Route("/", name="home")
-         */
         return $this->render('home.html.twig', [
             'foo' => 'Hello world !'
         ]);

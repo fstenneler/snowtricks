9b10ce351a116ad08223b3e895b290c2b5afe470
fstenneler <fabien.stenneler@gmail.com>
2019-10-07 16:45:15 +0200

    add entity trick, category, media and their fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use App\Entity\Trick;
+use App\Entity\Media;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+
+        // Create 5 categories
+        for ($i = 1; $i <= 5; $i++) {
+            $category = new Category();
+            $category->setName('Category ' . $i);            
+            $manager->persist($category);
+            $this->addReference('category-' . $i, $category);
+        }
+
+
+        // Create 50 tricks
+        for ($iTrick = 1; $iTrick <= 50; $iTrick++) {
+
+            // Trick
+            $trick = new Trick();
+            $trick->setName('Trick '.$iTrick);
+            $description = "Trick " . $iTrick . "\n\n";
+            $description .= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin placerat tincidunt molestie. Nam in neque nulla. Duis augue massa, blandit eget sapien sed, bibendum efficitur neque. Vestibulum ut pellentesque orci, vel feugiat nulla. Fusce elementum diam nec neque tincidunt, ac euismod mauris fermentum. Etiam at nunc sagittis, varius felis a, luctus felis. Ut ac auctor ligula. Maecenas vitae urna lacinia, posuere purus in, pulvinar mauris. Aliquam id sapien imperdiet, dignissim odio sit amet, tincidunt est. Maecenas mattis, nunc quis tincidunt congue, sapien nunc gravida velit, vitae placerat sapien leo nec tellus. Donec vel tempus ligula. Morbi condimentum ultricies nulla, at pulvinar erat tincidunt a. Nulla massa justo, bibendum vel rhoncus sed, feugiat mollis metus. Pellentesque imperdiet molestie lacus, a tincidunt nunc posuere vitae. Quisque vitae dapibus tellus.\n\n";
+            $description .= "Proin ac risus a enim ultrices aliquam. Curabitur auctor lectus ex, id tincidunt metus varius ac. Morbi felis tellus, iaculis in odio eu, accumsan tincidunt nunc. Cras maximus ante ex, vel vehicula massa ultricies sit amet. Sed in convallis dui. Pellentesque nec libero id nisl lobortis luctus. Mauris aliquet turpis commodo odio blandit, in vulputate magna aliquam. Maecenas vitae ultricies enim. Etiam augue felis, suscipit nec urna sed, imperdiet gravida nisi.\n\n";                 
+            $description .= "Sed et lorem rhoncus massa eleifend aliquet. Nulla pulvinar arcu quis congue molestie. Fusce eget fringilla tortor, nec iaculis diam. Mauris nec orci eu nisl facilisis lacinia. Sed vestibulum velit ac dui lacinia, et sagittis enim aliquet. Ut vel mi id nibh maximus dignissim ut in sapien. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n\n";                        
+            $description .= "Nulla non faucibus tortor, in pharetra dolor. Ut lobortis turpis vitae lorem fringilla, at rhoncus nisl congue. Fusce tincidunt consectetur tempor. Quisque a sem non lacus pharetra semper at elementum leo. Integer et eros nisi. Vivamus ut euismod mauris. Praesent maximus justo est, id vestibulum enim hendrerit sed. In mauris velit, porta quis tincidunt id, suscipit id sapien. Vivamus quis magna sit amet sapien rutrum feugiat sed ac neque. Sed rutrum quam vehicula, sagittis lectus vel, sodales justo. Aenean non augue vitae nisl molestie congue a eget lectus. Donec condimentum, dolor a euismod congue, turpis lectus tristique enim, ullamcorper cursus velit metus ut massa. Maecenas ornare nec leo vel dapibus.\n\n";                        
+            $description .= "Mauris dapibus sollicitudin iaculis. Phasellus eu ornare nisi. Aliquam posuere ac metus et hendrerit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec felis justo, hendrerit sit amet nunc eget, dictum consequat purus. Sed id ante a tellus pretium aliquam. Donec quam massa, dictum quis erat ac, euismod eleifend nulla. Nulla ante tellus, blandit eu suscipit ac, ornare eu lorem. Maecenas maximus eros at justo porttitor consectetur. Nunc vehicula orci egestas, interdum augue id, aliquet ex. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec cursus libero eget massa venenatis, congue lacinia odio faucibus. Fusce placerat, risus a iaculis commodo, mauris enim venenatis risus, non eleifend massa erat in ipsum.\n\n";
+            $trick->setDescription($description);
+            $trick->setCreationDate(
+                new \DateTime(
+                    date(
+                        'Y-m-d\TH:i:s.u', 
+                        mt_rand(
+                            mktime(0,0,0,1,1,2019),
+                            time()
+                        )
+                    )
+                )
+            );
+            $trick->setCategory($this->getReference('category-' . rand(1,5)));
+            $manager->persist($trick);
+            $this->addReference('trick-' . $iTrick, $trick);
+
+            //Media
+
+            // Create 3 pictures on Media
+            for ($i = 1; $i <= 3; $i++) {
+                $media = new Media();
+                $media->setUrl('trick_media.jpg');
+                $media->setTrick($this->getReference('trick-' . $iTrick));
+                if($i === 1) {
+                    $media->setIsHeader(true);
+                } else {
+                    $media->setIsHeader(false);
+                }                
+                $manager->persist($media);
+            }
+
+            // Create 3 videos on Media
+            for ($i = 1; $i <= 3; $i++) {
+                $media = new Media();
+                $media->setUrl('https://youtu.be/hPuVJkw1MmI');
+                $media->setTrick($this->getReference('trick-' . $iTrick));
+                $media->setIsHeader(false);
+                $manager->persist($media);
+            }
+
+        }
+
+        $manager->flush();
+    }
+
+}
---
+++ src/Entity/Trick.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TrickRepository")
+ */
+class Trick
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $creationDate;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $modificationDate;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Media", mappedBy="trick", orphanRemoval=true)
+     */
+    private $media;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Category")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
+    public function __construct()
+    {
+        $this->media = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreationDate(): ?\DateTimeInterface
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate(\DateTimeInterface $creationDate): self
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    public function getModificationDate(): ?\DateTimeInterface
+    {
+        return $this->modificationDate;
+    }
+
+    public function setModificationDate(?\DateTimeInterface $modificationDate): self
+    {
+        $this->modificationDate = $modificationDate;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Media[]
+     */
+    public function getMedia(): Collection
+    {
+        return $this->media;
+    }
+
+    public function addMedium(Media $medium): self
+    {
+        if (!$this->media->contains($medium)) {
+            $this->media[] = $medium;
+            $medium->setTrick($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMedium(Media $medium): self
+    {
+        if ($this->media->contains($medium)) {
+            $this->media->removeElement($medium);
+            // set the owning side to null (unless already changed)
+            if ($medium->getTrick() === $this) {
+                $medium->setTrick(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
